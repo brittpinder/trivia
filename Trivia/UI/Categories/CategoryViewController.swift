834e57117f11d3555e2779f5d7f2d37c0f9a92d4
Brittany Pinder <brittpinder@gmail.com>
2023-04-19 09:06:40 -0300

    Remove unnecessary unwrapping

--- Trivia/UI/Categories/CategoryViewController.swift
+++ Trivia/UI/Categories/CategoryViewController.swift
@@ -60,12 +60,12 @@ extension CategoryViewController {
 
         view.addSubview(collectionView)
 
-        collectionView!.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            collectionView!.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            collectionView!.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            collectionView!.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            collectionView!.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
     }
 }
