89d02d628cf1a77192cae5886542fa3e26375a14
Brittany Pinder <brittpinder@gmail.com>
2023-05-16 15:14:07 -0300

    Add CategoryDto tests

---
+++ TriviaTests/CategoryDtoTests.swift
@@ -0,0 +1,73 @@
+//
+//  CategoryDtoTests.swift
+//  TriviaTests
+//
+//  Created by Brittany Pinder on 2023-05-16.
+//
+
+import XCTest
+@testable import Trivia
+
+final class CategoryDtoTests: XCTestCase {
+
+    func testValidCategoryDto() throws {
+        let json = """
+        {
+             "id": 9,
+             "name": "Science"
+        }
+        """
+
+        let data = json.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+
+        let categoryDto = try decoder.decode(CategoryDto.self, from: data)
+
+        XCTAssertEqual(categoryDto.id, 9)
+        XCTAssertEqual(categoryDto.name, "Science")
+    }
+
+    func testCategoryDtoFailsWithoutId() throws {
+        let json = """
+        {
+             "name": "Science"
+        }
+        """
+
+        let data = json.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+
+        XCTAssertThrowsError(try decoder.decode(CategoryDto.self, from: data))
+    }
+
+    func testCategoryDtoFailsWithoutName() throws {
+        let json = """
+        {
+             "id": 9
+        }
+        """
+
+        let data = json.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+
+        XCTAssertThrowsError(try decoder.decode(CategoryDto.self, from: data))
+    }
+
+    func testCategoryDtoFailsWithInvalidIdType() throws {
+        let json = """
+        {
+             "id": "9",
+             "name": "Science"
+        }
+        """
+
+        let data = json.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+
+        XCTAssertThrowsError(try decoder.decode(CategoryDto.self, from: data))
+    }
+}
