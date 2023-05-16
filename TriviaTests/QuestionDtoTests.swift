f55956f3e9668b6188e694ba2012de05c2c7fa2c
Brittany Pinder <brittpinder@gmail.com>
2023-05-16 15:07:50 -0300

    Add unit tests for QuestionDto

---
+++ TriviaTests/QuestionDtoTests.swift
@@ -0,0 +1,121 @@
+//
+//  QuestionDtoTests.swift
+//  TriviaTests
+//
+//  Created by Brittany Pinder on 2023-05-16.
+//
+
+import XCTest
+@testable import Trivia
+
+final class QuestionDtoTests: XCTestCase {
+
+    func testValidQuestionDto() throws {
+        let json = """
+        {
+             "category": "Geography",
+             "question": "What is the capital of Canada?",
+             "correct_answer": "Ottawa",
+             "incorrect_answers": [
+                 "Toronto",
+                 "Ontario",
+                 "Vancouver"
+             ]
+        }
+        """
+
+        let data = json.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+
+        let questionDto = try decoder.decode(QuestionDto.self, from: data)
+
+        XCTAssertEqual(questionDto.category, "Geography")
+        XCTAssertEqual(questionDto.question, "What is the capital of Canada?")
+        XCTAssertEqual(questionDto.correctAnswer, "Ottawa")
+        XCTAssertEqual(questionDto.incorrectAnswers, ["Toronto", "Ontario", "Vancouver"])
+    }
+
+    func testQuestionDtoFailsWithoutCategory() throws {
+        let json = """
+        {
+             "question": "What is the capital of Canada?",
+             "correct_answer": "Ottawa",
+             "incorrect_answers": [
+                 "Toronto",
+                 "Ontario",
+                 "Vancouver"
+             ]
+        }
+        """
+
+        let data = json.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+
+        XCTAssertThrowsError(try decoder.decode(QuestionDto.self, from: data))
+    }
+
+    func testQuestionDtoFailsWithoutQuestion() throws {
+        let json = """
+        {
+             "category": "Geography",
+             "correct_answer": "Ottawa",
+             "incorrect_answers": [
+                 "Toronto",
+                 "Ontario",
+                 "Vancouver"
+             ]
+        }
+        """
+
+        let data = json.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+
+        XCTAssertThrowsError(try decoder.decode(QuestionDto.self, from: data))
+    }
+
+    func testQuestionDtoFailsWithMisspelledCorrectAnswer() throws {
+        let json = """
+        {
+             "category": "Geography",
+             "question": "What is the capital of Canada?",
+             "correctAnswer": "Ottawa",
+             "incorrect_answers": [
+                 "Toronto",
+                 "Ontario",
+                 "Vancouver"
+             ]
+        }
+        """
+
+        let data = json.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+
+        XCTAssertThrowsError(try decoder.decode(QuestionDto.self, from: data))
+    }
+
+    func testQuestionDtoFailsWithMisspelledIncorrectAnswers() throws {
+        let json = """
+        {
+             "category": "Geography",
+             "question": "What is the capital of Canada?",
+             "correct_answer": "Ottawa",
+             "incorrectanswers": [
+                 "Toronto",
+                 "Ontario",
+                 "Vancouver"
+             ]
+        }
+        """
+
+        let data = json.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+
+        XCTAssertThrowsError(try decoder.decode(QuestionDto.self, from: data))
+    }
+
+}
