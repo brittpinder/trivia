9996571c4bf6c9dd4dbb905e7276859a1bc96b54
Brittany Pinder <brittpinder@gmail.com>
2023-05-24 16:43:57 -0300

    Remove unnecessary init overrides

--- Trivia/UI/MainViewController.swift
+++ Trivia/UI/MainViewController.swift
@@ -28,15 +28,6 @@ class MainViewController: UIViewController {
         return alert
     }()
 
-    init() {
-        categoryViewController = CategoryViewController(triviaService: triviaService)
-        super.init(nibName: nil, bundle: nil)
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
